8ad7c48e73b39775e16ac480832ff5e7db08c184
ChrisLook <LookWChristopher@gmail.com>
2017-04-26 22:58:28 -0400

    descriptor.csv is the descriptor file, look at that to see whats wrong with the data

--- combined_learn.py
+++ combined_learn.py
@@ -1,8 +1,4 @@
-import nltk
 import random
-from nltk.corpus import movie_reviews
-from nltk.classify.scikitlearn import SklearnClassifier
-import pickle
 
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB
 from sklearn.linear_model import LogisticRegression, SGDClassifier
@@ -10,13 +6,12 @@ from sklearn.svm import SVC, LinearSVC, NuSVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 
-from nltk.classify import ClassifierI
 from statistics import mode
 import csv
 import ast
 import numpy as np
 import sys
-import os
+import o
 import datetime
 import codecs
 
@@ -66,18 +61,18 @@ class VoteClassifier(ClassifierI):
 # now = datetime.datetime.now()
 
 #open prediction and results file to write to
-results_file=open('../Results','a')
+results_file=open('Results','a')
 result_writer=csv.writer(results_file,delimiter=',')
-files = os.walk(data_dir).next()[2]
+#files = os.walk(data_dir).next()[2]
 
-predictions_writer=csv.writer(predictions_file,delimiter=',')
-predictions_writer.writerow(["date","symbol","Pred","Conf"])
+# predictions_writer=csv.writer(predictions_file,delimiter=',')
+# predictions_writer.writerow(["date","symbol","Pred","Conf"])
 #write identifier at start of results file
-if identifier!="0":
-  result_writer.writerow([now,identifier])
+# if identifier!="0":
+#   result_writer.writerow([now,identifier])
 predictions=[]
 #perform cross validation
-data_file="replace with something actually later"
+data_file="test.csv"
 #get descriptor data from files
 descriptor_file=open(data_file,'r')
 descriptor_reader=csv.reader(descriptor_file,delimiter='\t')
@@ -92,73 +87,73 @@ elements=len(featuresets)
 normal_acc=[]
 #calculate indicies to split by for cross validaton
 for i in range(0,num_folds):
-first_index=int((i*elements/float(num_folds)))
-second_index=int(((i+1)*elements/float(num_folds)))
-testing_set=[]
-training_set=[]
-dates=[]
-blind_testing_set=[]
-true=[]
-#split the sets into training and testing sets
-for n in (featuresets[:first_index]+featuresets[second_index:]):#adding training data and +/- for 
-  training_set.append(n[0:-2],n[-1]])
-for n in (featuresets[first_index:second_index]):
-  testing_set.append([n[0:-2],n[-1]])
+	first_index=int((i*elements/float(num_folds)))
+	second_index=int(((i+1)*elements/float(num_folds)))
+	testing_set=[]
+	training_set=[]
+	dates=[]
+	blind_testing_set=[]
+	true=[]
+	#split the sets into training and testing sets
+	for n in (featuresets[:first_index]+featuresets[second_index:]):#adding training data and +/- for 
+	  training_set.append([n[0:-2],n[-1]])
+	for n in (featuresets[first_index:second_index]):
+	  testing_set.append([n[0:-2],n[-1]])
 
-# for n in testing_set:
-  # blind_testing_set.append(n[0])
-  # true.append(n[1])
- ######################################train data
+	# for n in testing_set:
+	  # blind_testing_set.append(n[0])
+	  # true.append(n[1])
+	 ######################################train data
 
-##classifier=nltk.NaiveBayesClassifier.train(training_set)
-MNB_classifier = SklearnClassifier(MultinomialNB())
-MNB_classifier.train(training_set)
+	##classifier=nltk.NaiveBayesClassifier.train(training_set)
+	MNB_classifier = SklearnClassifier(MultinomialNB())
+	MNB_classifier.train(training_set)
 
-#Maxent_classifier = SklearnClassifier(MaxentClassifier())
-#Maxent_classifier.train(training_set)
+	#Maxent_classifier = SklearnClassifier(MaxentClassifier())
+	#Maxent_classifier.train(training_set)
 
-BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
-BernoulliNB_classifier.train(training_set)
+	BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
+	BernoulliNB_classifier.train(training_set)
 
-RandomForest_classifier = SklearnClassifier(RandomForestClassifier(n_estimators=100))
-RandomForest_classifier.train(training_set)
+	RandomForest_classifier = SklearnClassifier(RandomForestClassifier(n_estimators=100))
+	RandomForest_classifier.train(training_set)
 
-LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
-LogisticRegression_classifier.train(training_set)
+	LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
+	LogisticRegression_classifier.train(training_set)
 
-SGDClassifier_classifier = SklearnClassifier(SGDClassifier())
-SGDClassifier_classifier.train(training_set)
+	SGDClassifier_classifier = SklearnClassifier(SGDClassifier())
+	SGDClassifier_classifier.train(training_set)
 
-SVC_classifier = SklearnClassifier(SVC(kernel='rbf'))
-SVC_classifier.train(training_set)
+	SVC_classifier = SklearnClassifier(SVC(kernel='rbf'))
+	SVC_classifier.train(training_set)
 
-LinearSVC_classifier = SklearnClassifier(LinearSVC())
-LinearSVC_classifier.train(training_set)
+	LinearSVC_classifier = SklearnClassifier(LinearSVC())
+	LinearSVC_classifier.train(training_set)
 
-NearestNeighbors_classifier=SklearnClassifier(KNeighborsClassifier(n_neighbors=2, algorithm='ball_tree'))
-NearestNeighbors_classifier.train(training_set)
+	NearestNeighbors_classifier=SklearnClassifier(KNeighborsClassifier(n_neighbors=2, algorithm='ball_tree'))
+	NearestNeighbors_classifier.train(training_set)
 
-# NuSVC_classifier = SklearnClassifier(NuSVC())
-# NuSVC_classifier.train(training_set)
-voted_classifier = VoteClassifier(
-	RandomForest_classifier,
-									SGDClassifier_classifier,
-									LogisticRegression_classifier,BernoulliNB_classifier,MNB_classifier,NearestNeighbors_classifier,SVC_classifier)
-normal_acc.append((nltk.classify.accuracy(voted_classifier, testing_set))*(len(testing_set)/float(elements)))
-print len(testing_set)
-plusminus=voted_classifier.classify_many(blind_testing_set)
+	# NuSVC_classifier = SklearnClassifier(NuSVC())
+	# NuSVC_classifier.train(training_set)
+	voted_classifier = VoteClassifier(
+		RandomForest_classifier,
+										SGDClassifier_classifier,
+										LogisticRegression_classifier,BernoulliNB_classifier,MNB_classifier,NearestNeighbors_classifier,SVC_classifier)
+	normal_acc.append((nltk.classify.accuracy(voted_classifier, testing_set))*(len(testing_set)/float(elements)))
+	print len(testing_set)
+	plusminus=voted_classifier.classify_many(blind_testing_set)
 
-# for i in range(len(dates)):
-  # predictions.append([dates[i],comp_name,str(plusminus[i]),voted_classifier.confidence(blind_testing_set[i])])  
-  # #predictions_writer.writerow([dates[i],comp_name,str(plusminus[i])])
-  # print [plusminus[i],true[i]]
+	# for i in range(len(dates)):
+	  # predictions.append([dates[i],comp_name,str(plusminus[i]),voted_classifier.confidence(blind_testing_set[i])])  
+	  # #predictions_writer.writerow([dates[i],comp_name,str(plusminus[i])])
+	  # print [plusminus[i],true[i]]
 
-  
-  #print predictions
+	  
+	  #print predictions
 print("voted_classifier accuracy percent:", (nltk.classify.accuracy(voted_classifier, testing_set))*100)
 print sum(normal_acc)
 if identifier!=0:
-result_writer.writerow([comp_name,sum(normal_acc)])
+	result_writer.writerow([comp_name,sum(normal_acc)])
 
     # print("Classification:", voted_classifier.classify(testing_set[0][0]), "Confidence %:",voted_classifier.confidence(testing_set[0][0])*100)
 # print("Classification:", voted_classifier.classify(testing_set[1][0]), "Confidence %:",voted_classifier.confidence(testing_set[1][0])*100)
