2108a7f5eccc5927195688a404a153ae74bbf903
LookWChristopher@gmail.com <LookWChristopher@gmail.com>
2017-04-30 23:09:04 -0400

    updating s test1

--- MatLabPrograms/fileIO.m
+++ MatLabPrograms/fileIO.m
@@ -1,7 +1,7 @@
 
 
 
-cd ..\HC_csv
+cd ..\AD_csv
  dirs = dir(fullfile('.','*.xlsx'));
     for  direc =dirs'
     delimiterIn = ',';
@@ -11,13 +11,13 @@ cd ..\HC_csv
     for file = files0'
         filename = file.name;
         stimulusTonefile=fullfile(baseFileName,filename)
-        stimP50= erp1(stimulusTonefile,24,72,'max');
-        stimN100=erp1(stimulusTonefile,70,130,'min');
-        stimP200=erp1(stimulusTonefile,180,235,'max');
-        stimN200=erp1(stimulusTonefile,205,315,'min');
+        stimP50= erp2(stimulusTonefile,24,72,'max');
+        stimN100=erp2(stimulusTonefile,70,130,'min');
+        stimP200=erp2(stimulusTonefile,180,235,'max');
+        stimN200=erp2(stimulusTonefile,205,315,'min');
         stimP3a= erp(stimulusTonefile,325,500,'max');
-        stimP3b= erp1(stimulusTonefile,325,580,'max');
-        stimSlow=erp1(stimulusTonefile,460,680,'min');
+        stimP3b= erp2(stimulusTonefile,325,580,'max');
+        stimSlow=erp2(stimulusTonefile,460,680,'min');
         lstimN200=lat(stimulusTonefile,205,315,'n200');
         lstimSlow=lat(stimulusTonefile,460,680,'slow');
         lstimP3b= lat(stimulusTonefile,325,580,'p3b');
@@ -27,13 +27,13 @@ cd ..\HC_csv
     for file = files1'
         filename = file.name;
         targetTonefile=fullfile(baseFileName,filename)
-        targP50= erp1(targetTonefile,24,72,'max');
-        targN100=erp1(targetTonefile,70,130,'min');
-        targP200=erp1(targetTonefile,180,235,'max');
-        targN200=erp1(targetTonefile,205,315,'min');
-        targP3a= erp1(targetTonefile,325,500,'max');
-        targP3b= erp1(targetTonefile,325,580,'max');
-        targSlow=erp1(targetTonefile,460,680,'min');
+        targP50= erp2(targetTonefile,24,72,'max');
+        targN100=erp2(targetTonefile,70,130,'min');
+        targP200=erp2(targetTonefile,180,235,'max');
+        targN200=erp2(targetTonefile,205,315,'min');
+        targP3a= erp2(targetTonefile,325,500,'max');
+        targP3b= erp2(targetTonefile,325,580,'max');
+        targSlow=erp2(targetTonefile,460,680,'min');
         ltargN200=lat(targetTonefile,205,315,'n200');
         ltargSlow=lat(targetTonefile,460,680,'slow');
         ltargP3b= lat(targetTonefile,325,580,'p3b');
@@ -44,20 +44,20 @@ cd ..\HC_csv
     for file = files2'
         filename = file.name;
         distractorTonefile=fullfile(baseFileName,filename)
-        distP50= erp1(distractorTonefile,24,72,'max');
-        distN100=erp1(distractorTonefile,70,130,'min');
-        distP200=erp1(distractorTonefile,180,235,'max');
-        distN200=erp1(distractorTonefile,205,315,'min');
-        distP3a= erp1(distractorTonefile,325,500,'max');
-        distP3b= erp1(distractorTonefile,325,580,'max');
-        distSlow=erp1(distractorTonefile,460,680,'min');
+        distP50= erp2(distractorTonefile,24,72,'max');
+        distN100=erp2(distractorTonefile,70,130,'min');
+        distP200=erp2(distractorTonefile,180,235,'max');
+        distN200=erp2(distractorTonefile,205,315,'min');
+        distP3a= erp2(distractorTonefile,325,500,'max');
+        distP3b= erp2(distractorTonefile,325,580,'max');
+        distSlow=erp2(distractorTonefile,460,680,'min');
         ldistN200=lat(distractorTonefile,205,315,'n200');
         ldistSlow=lat(distractorTonefile,460,680,'slow');
         ldistP3b= lat(distractorTonefile,325,580,'p3b');
         descriptor=[descriptor, distP50,distN100,distP200,distN200,distP3a,distP3b,distSlow,ldistN200,ldistSlow,ldistP3b];
         
     end
-    descriptor=[descriptor,0];
+    descriptor=[descriptor,1];
     dlmwrite('..\test1.csv',descriptor,'delimiter',',','-append');
     end
 cd ..\MatLabPrograms
--- MatLabPrograms/fileIO_n.m
+++ MatLabPrograms/fileIO_n.m
@@ -11,13 +11,13 @@ cd ..\HC_csv
     for file = files0'
         filename = file.name;
         stimulusTonefile=fullfile(baseFileName,filename)
-        stimP50= erp1(stimulusTonefile,24,72,'max');
-        stimN100=erp1(stimulusTonefile,70,130,'min')-stimP50;
-        stimP200=erp1(stimulusTonefile,180,235,'max')-stimP50;
-        stimN200=erp1(stimulusTonefile,205,315,'min')-stimP50;
+        stimP50= erp2(stimulusTonefile,24,72,'max');
+        stimN100=erp2(stimulusTonefile,70,130,'min')-stimP50;
+        stimP200=erp2(stimulusTonefile,180,235,'max')-stimP50;
+        stimN200=erp2(stimulusTonefile,205,315,'min')-stimP50;
         stimP3a= erp(stimulusTonefile,325,500,'max')-stimP50;
-        stimP3b= erp1(stimulusTonefile,325,580,'max')-stimP50;
-        stimSlow=erp1(stimulusTonefile,460,680,'min')-stimP50;
+        stimP3b= erp2(stimulusTonefile,325,580,'max')-stimP50;
+        stimSlow=erp2(stimulusTonefile,460,680,'min')-stimP50;
         lstimN200=lat(stimulusTonefile,205,315,'n200');
         lstimSlow=lat(stimulusTonefile,460,680,'slow');
         lstimP3b= lat(stimulusTonefile,325,580,'p3b');
@@ -27,13 +27,13 @@ cd ..\HC_csv
     for file = files1'
         filename = file.name;
         targetTonefile=fullfile(baseFileName,filename)
-        targP50= erp1(targetTonefile,24,72,'max');
-        targN100=erp1(targetTonefile,70,130,'min')-targP50;
-        targP200=erp1(targetTonefile,180,235,'max')-targP50;
-        targN200=erp1(targetTonefile,205,315,'min')-targP50;
-        targP3a= erp1(targetTonefile,325,500,'max')-targP50;
-        targP3b= erp1(targetTonefile,325,580,'max')-targP50;
-        targSlow=erp1(targetTonefile,460,680,'min')-targP50;
+        targP50= erp2(targetTonefile,24,72,'max');
+        targN100=erp2(targetTonefile,70,130,'min')-targP50;
+        targP200=erp2(targetTonefile,180,235,'max')-targP50;
+        targN200=erp2(targetTonefile,205,315,'min')-targP50;
+        targP3a= erp2(targetTonefile,325,500,'max')-targP50;
+        targP3b= erp2(targetTonefile,325,580,'max')-targP50;
+        targSlow=erp2(targetTonefile,460,680,'min')-targP50;
         ltargN200=lat(targetTonefile,205,315,'n200');
         ltargSlow=lat(targetTonefile,460,680,'slow');
         ltargP3b= lat(targetTonefile,325,580,'p3b');
@@ -44,13 +44,13 @@ cd ..\HC_csv
     for file = files2'
         filename = file.name;
         distractorTonefile=fullfile(baseFileName,filename)
-        distP50= erp1(distractorTonefile,24,72,'max');
-        distN100=erp1(distractorTonefile,70,130,'min')-distP50;
-        distP200=erp1(distractorTonefile,180,235,'max')-distP50;
-        distN200=erp1(distractorTonefile,205,315,'min')-distP50;
-        distP3a= erp1(distractorTonefile,325,500,'max')-distP50;
-        distP3b= erp1(distractorTonefile,325,580,'max')-distP50;
-        distSlow=erp1(distractorTonefile,460,680,'min')-distP50;
+        distP50= erp2(distractorTonefile,24,72,'max');
+        distN100=erp2(distractorTonefile,70,130,'min')-distP50;
+        distP200=erp2(distractorTonefile,180,235,'max')-distP50;
+        distN200=erp2(distractorTonefile,205,315,'min')-distP50;
+        distP3a= erp2(distractorTonefile,325,500,'max')-distP50;
+        distP3b= erp2(distractorTonefile,325,580,'max')-distP50;
+        distSlow=erp2(distractorTonefile,460,680,'min')-distP50;
         ldistN200=lat(distractorTonefile,205,315,'n200');
         ldistSlow=lat(distractorTonefile,460,680,'slow');
         ldistP3b= lat(distractorTonefile,325,580,'p3b');
@@ -58,6 +58,6 @@ cd ..\HC_csv
         
     end
     descriptor=[descriptor,0];
-    dlmwrite('..\test1.csv',descriptor,'delimiter',',','-append');
+    dlmwrite('..\test2.csv',descriptor,'delimiter',',','-append');
     end
 cd ..\MatLabPrograms
