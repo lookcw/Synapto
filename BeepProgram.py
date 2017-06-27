1366dc4aa8bf304f78d421b0b343b5bae9e09666
Megha Guggari <mguggari@terpmail.umd.edu>
2017-06-26 23:04:02 -0400

    Update BeepProgram.py

--- BeepProgram.py
+++ BeepProgram.py
@@ -1,19 +1,60 @@
+#Beep Program
+
 import winsound
 import random
 
+#Baseline is 500 Hz and Oddball is 1000 Hz.
+#Duration (Dur) is the number of milliseconds that the sound is played.
 Baseline = 500
 Oddball = 1000
-Dur = 1000
+Duration = 10
 
-y = range(0,100,5)
+#a is 200 numbers starting from 6 (starting from 6 because we don't
+#want the first 5 beeps to be oddball. To avoid consecutive number
+#generation, increment range by 2.
+#z gives you 40 random numbers from the range of 200 numbers.
+#numOddballBeeps is greater than the total number of oddball beeps we want to hear
+#because we want a large range of numbers in case the oddball beep gets skipped.
+numOddballBeeps = 40
+a = range(6, 200)
+z = random.sample(a, numOddballBeeps)
+print(z)
 
-for counter in range(120):
-    x = random.randint(0,100)
-    print(x)
-    if x not in list(y) or counter<5:
-        winsound.Beep(Baseline,Dur)
+numBeeps = 300
+maxOddballBeeps = 0
+normalBeeps = 0
+counter = 0
+oddballCounter = 0
+oddballPlayed = 0
+
+while counter in range(numBeeps):
+    if counter not in list(z):
+        winsound.Beep(Baseline,Duration)
+        normalBeeps += 1
+        oddballPlayed = 0
     else:
-        winsound.Beep(Oddball,Dur)
+        if(oddballPlayed == 0 and maxOddballBeeps < 24):
+            winsound.Beep(Oddball,Duration)
+            maxOddballBeeps += 1
+            oddballPlayed = 1
+            print(counter)
+        else:                       #Makes sure that consecutive beeps aren't played.
+            winsound.Beep(Baseline,Duration) 
+            print("odd ball skipped") 
+    counter +=1
+    if (normalBeeps + maxOddballBeeps >= 200):
+        break
+    
+print(maxOddballBeeps)
+print(normalBeeps)
+
+
+
+
+    
+
+
+
 
 
 
