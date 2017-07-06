c3e561c1ac930e0885241dcfb601102fc17421d0
ChrisLook <LookWChristopher@gmail.com>
2017-07-05 22:13:54 -0400

    beep program working

--- BeepProgram.py
+++ BeepProgram.py
@@ -2,24 +2,43 @@
 from __future__ import division
 import random
 import math
-from pyaudio import PyAudio # sudo apt-get install python{,3}-pyaudio
-import pyglet
-
+import pygame as pg
 try:
     from itertools import izip
 except ImportError: # Pytho 3
     izip = zip
     xrange = range
 
-def exit_callback(dt):
-    pyglet.app.exit()
-
-pyglet.clock.schedule_once(exit_callback , 1000)
-player = pyglet.media.Player()
 
 
-norm_tone = pyglet.media.load('500.wav',streaming=False)
-oddball = pyglet.media.load('1000.wav',streaming=False)
+
+
+
+def play_music(music_file, volume=0.8):
+    '''
+    stream music with mixer.music module in a blocking manner
+    this will stream the sound from disk while playing
+    '''
+    # set up the mixer
+    freq = 44100     # audio CD quality
+    bitsize = -16    # unsigned 16 bit
+    channels = 2     # 1 is mono, 2 is stereo
+    buffer = 2048    # number of samples (experiment to get best sound)
+    pg.mixer.init(freq, bitsize, channels, buffer)
+    # volume value 0.0 to 1.0
+    pg.mixer.music.set_volume(volume)
+    clock = pg.time.Clock()
+    try:
+        pg.mixer.music.load(music_file)
+        print("Music file {} loaded!".format(music_file))
+    except pg.error:
+        print("File {} not found! ({})".format(music_file, pg.get_error()))
+        return
+    pg.mixer.music.play()
+    while pg.mixer.music.get_busy():
+        # check if playback has finished
+        clock.tick(1)
+
 #Baseline is 500 Hz and Oddball is 1000 Hz.
 #Duration is the number of milliseconds that the sound is played.
 Baseline = 500
@@ -42,30 +61,22 @@ normalBeeps = 0
 counter = 0
 oddballCounter = 0
 oddballPlayed = 0
-norm_tone.play()
 while counter in range(numBeeps):
     if counter not in list(z):
-        print "print whatever"
-        pyglet.clock.schedule_once(exit_callback , 1)
-        norm_tone.play()
-        pyglet.app.run()
-        print "cry"
+        play_music('500.wav',0.8)
+        print "standard"
 
         normalBeeps += 1
         oddballPlayed = 0
     else:
         if(oddballPlayed == 0 and maxOddballBeeps < 24):
-            pyglet.clock.schedule_once(exit_callback , 1)
-            oddball.play()
-            pyglet.app.run()
-            print "oddball played"
+            play_music('1000.wav',0.8)
+            print "oddball"
             maxOddballBeeps += 1
             oddballPlayed = 1
             print(counter)
         else:                       #Makes sure that consecutive beeps aren't played.
-            pyglet.clock.schedule_once(exit_callback , 1)
-            norm_tone.play()
-            pyglet.app.run()
+            play_music('1000.wav',0.8)
             print("odd ball skipped") 
     counter +=1
     if (normalBeeps + maxOddballBeeps >= 200):
