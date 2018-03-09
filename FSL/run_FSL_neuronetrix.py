fd418b3447400c7ed5070371d0ed239cdd81f9f5
lookcw <clook@terpmail.umd.edu>
2018-03-09 11:55:47 -0500

    adding fsl code

--- FSL/run_FSL_neuronetrix.py
+++ FSL/run_FSL_neuronetrix.py
@@ -4,8 +4,8 @@ from subprocess import call
 
 
 
-data_dirs = ["../Data/AD_BD/","../Data/HC_BD"]
-output_dir = "../Data/Brazil_FSL/" 
+data_dirs = ["../Data/AD_BD_Fil/","../Data/HC_BD_Fil"]
+output_dir = "Brazil_FSL/" 
 count = 0
 for data_dir in data_dirs:
 	for filename in (os.listdir(data_dir)):
@@ -15,6 +15,7 @@ for data_dir in data_dirs:
 			label = "AD"
 		else:
 			label = "HC"
-		file = output_dir + label + "_" + filename.split(" ")[0] + "_" + filename.split(" ")[1] + ".dat"
+		file = output_dir + label + "_" + filename.split("Fil")[0] + "_" + filename.split("Fil")[1].replace(".txt","") + ".dat"
 		#./FSL -l 1 -m 10 -p .049 -s 1 -x 100 -w 410 -i input file -o test.dat
-		call(["./FSL","-l", "1", "-m", "10", "-p", "0.049", "-s", "1", "-x", "100", "-w", "410", "-i", os.path.join(data_dir,filename),"-o",file]) 
+		if not os.path.exists(file):
+			call(["./FSL","-l", "1", "-m", "10", "-p", "0.049", "-s", "1", "-x", "100", "-w", "410", "-i", os.path.join(data_dir,filename),"-o",file]) 
