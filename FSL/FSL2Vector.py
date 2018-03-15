593d096e87f67c1951e67b22614084dea7e6545d
lookcw <clook@terpmail.umd.edu>
2018-03-15 16:10:31 -0400

    i want to kms

--- FSL/FSL2Vector.py
+++ FSL/FSL2Vector.py
@@ -6,12 +6,13 @@ vec_dir = "Brazil_FSL_Features"
 for filename in (os.listdir(data_dir)):
 	vec = []
 	reader = csv.reader(open(os.path.join(data_dir,filename),'r'),delimiter = ' ')
-	writer = csv.writer(open(os.path.join(vec_dir,filename.split('.')[0]+".csv"),delimiter = ','))
+	writer = csv.writer(open(os.path.join(vec_dir,filename.split('.')[0]+".csv"),'w'),delimiter = ',')
 	mat = list(reader)
+	print len(mat)
 	for i in range(len(mat)):
 		for j in range(len(mat[0])):
-			if j > i: #gets upper triangular matrix 
+			if j > i+1: #gets upper triangular matrix 
 				vec.append(mat[i][j])
 	writer.writerow(vec)
-	
+
 
--- FSL/run_FSL_neuronetrix.py
+++ FSL/run_FSL_neuronetrix.py
@@ -4,18 +4,20 @@ from subprocess import call
 
 
 
-data_dirs = ["../Data/AD_BD_Fil/","../Data/HC_BD_Fil"]
-output_dir = "Brazil_FSL/" 
+data_dirs = ["../Data/AD/","../Data/HC/"]
+output_dir = "Brazil_FSL/"
+bands = ["Alpha","Theta","Gamma","Beta","Delta"]
 count = 0
 for data_dir in data_dirs:
-	for filename in (os.listdir(data_dir)):
-		if count == 1:
-			sys.exit(0)
-		if "AD" in data_dir:
-			label = "AD"
-		else:
-			label = "HC"
-		file = output_dir + label + "_" + filename.split("Fil")[0] + "_" + filename.split("Fil")[1].replace(".txt","") + ".dat"
-		#./FSL -l 1 -m 10 -p .049 -s 1 -x 100 -w 410 -i input file -o test.dat
-		if not os.path.exists(file):
-			call(["./FSL","-l", "1", "-m", "10", "-p", "0.049", "-s", "1", "-x", "100", "-w", "410", "-i", os.path.join(data_dir,filename),"-o",file]) 
+	for band in bands:
+		for filename in (os.listdir(os.path.join(data_dir,band))):
+			if count == 1:
+				sys.exit(0)
+			if "AD" in data_dir:
+				label = "AD"
+			else:
+				label = "HC"
+			file = os.path.join(output_dir,band) + "/" + filename.replace(".csv","") + ".dat"
+			#./FSL -l 1 -m 10 -p .049 -s 1 -x 100 -w 410 -i input file -o test.dat
+			if not os.path.exists(file):
+				call(["./FSL","-l", "1", "-m", "10", "-p", "0.049", "-s", "1", "-x", "100", "-w", "410", "-i", os.path.join(data_dir + "/" + band,filename),"-o",file]) 
