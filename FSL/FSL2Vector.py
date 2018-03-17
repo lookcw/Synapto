fc6355028e69ed56456acd65702da0254e9574ce
lookcw <clook@terpmail.umd.edu>
2018-03-17 19:59:02 -0400

    uploading bands FSL data

--- FSL/FSL2Vector.py
+++ FSL/FSL2Vector.py
@@ -3,16 +3,17 @@ import csv
 
 data_dir = "Brazil_FSL"
 vec_dir = "Brazil_FSL_Features"
-for filename in (os.listdir(data_dir)):
-	vec = []
-	reader = csv.reader(open(os.path.join(data_dir,filename),'r'),delimiter = ' ')
-	writer = csv.writer(open(os.path.join(vec_dir,filename.split('.')[0]+".csv"),'w'),delimiter = ',')
-	mat = list(reader)
-	print len(mat)
-	for i in range(len(mat)):
-		for j in range(len(mat[0])):
-			if j > i+1: #gets upper triangular matrix 
-				vec.append(mat[i][j])
-	writer.writerow(vec)
+for dir_name in (os.listdir(data_dir)):
+	for filename in (os.listdir(os.path.join(data_dir,dir_name))):
+		vec = []
+		reader = csv.reader(open(os.path.join( data_dir + "/" + dir_name  ,filename),'r'),delimiter = ' ')
+		writer = csv.writer(open(os.path.join(vec_dir + "/" +  dir_name ,filename.split('.')[0]+".csv"),'w'),delimiter = ',')
+		mat = list(reader)
+		print len(mat)
+		for i in range(len(mat)):
+			for j in range(len(mat[0])):
+				if j > i+1: #gets upper triangular matrix 
+					vec.append(mat[i][j])
+		writer.writerow(vec)
 
 
--- FSL/FSL2feature_set.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import csv
-
-data_dir = "Brazil_FSL/"
-for filename in (os.listdir(data_dir)):
-	f = open(os.path.join(data_dir,filename),'r')
-	reader = csv.reader(f,delimiter = ' ')
-	
