e193a68953849c4a726dfdf742b0d122b8e73ec6
lookcw <clook@terpmail.umd.edu>
2018-05-13 00:10:07 -0400

    adding random files

--- tensorflow/Brazil/normalize_2.py
+++ tensorflow/Brazil/normalize_2.py
@@ -4,39 +4,19 @@ import csv
 import sys
 from numpy import linalg as LA
 
-def norm(arr_in):
-	x_normed = (x - x.min(0)) / x.ptp(0)
-	print len(norm1)
-	for i in norm1:
-		if abs(i) > 1:
-			print "theres an issue"
-	return norm1
-
 def zscore_file(in_file,out_file,is_header):
 	f_in = open(in_file,'rb')
 	reader = csv.reader(f_in,delimiter=",")
 	# if (is_header):
 	# 	header = next(reader)
 	mat = np.array(list(reader))
-	print mat
-	ans = np.empty_like(mat.transpose())
-	mat = mat.transpose()
-	labels = mat[-1]
-	mat = np.delete(mat,len(mat)-1,0)
 	mat = mat.astype(np.float)
-	print len(mat)
-	count = 0
-	for i in range(len(mat)):
-		ans[i] = norm(mat[i])
-		count += 1
-		print count
-	ans[len(mat)] = labels
-	ans = ans.transpose()
+	x_normed = (mat - mat.min(0)) / mat.ptp(0)
 	f_out = open(out_file,'wb')
 	writer = csv.writer(f_out,delimiter=",")	
 	# if is_header:
 	# 	writer.writerow(header)
-	for row in ans:
+	for row in x_normed:
 		writer.writerow(row)
 		
 zscore_file(sys.argv[1],sys.argv[2],False)
