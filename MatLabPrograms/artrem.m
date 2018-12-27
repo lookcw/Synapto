d82ef47bab51b7b1516a0f2f18fd6d26c81526a4
Christopher Look <LookWChristopher@gmail.com>
2018-12-26 23:51:28 -0500

    cleaning up

--- MatLabPrograms/artrem.m
+++
@@ -1,7 +0,0 @@
-function [cleandata] = artrem(rawdata)
-[weights,sphere,~,~,~,~,~,~] = runica(rawdata);
-W = weights*sphere;
-activations = W*rawdata;
-Winv = inv(W);
-cleandata = Winv*activations;
-end
--- MatLabPrograms/run_resting_feature_gen.m
+++
@@ -1,2 +0,0 @@
-resting_feature_gen('../Lucas_Control',0)
-resting_feature_gen('../Lucas_Patients',1)
