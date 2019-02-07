0d56dc254ee7491724f8227e403d64e6a9d97e73
Anoop Patel <Anoop@wireless-10-105-17-60.umd.edu>
2019-02-06 22:10:33 -0500

    fixing merge conflicts

--- pipeline/FSL_features.py
+++ pipeline/FSL_features.py
@@ -8,9 +8,9 @@ def extractFSLFeatures(time_series_electrode):
 	if "TERM_PROGRAM" in os.environ:
 		p = Popen(["./FSL_mac","-l", "1", "-m", "10", "-p", "0.049", "-s", "1", "-x", "100", "-w", "410"]
 			, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-		inMat = "\n".join([','.join(x) for x in time_series_electrode])
+		inMat = "\n".join([' '.join(x) for x in time_series_electrode])
 		output, err = p.communicate(input= inMat)
-		mat = [s.strip().split(',') for s in output.strip().split('\n')]
+		mat = [s.strip().split(' ') for s in output.strip().split('\n')]
 	else:
 		p = Popen(["./FSL_linux","-l", "1", "-m", "10", "-p", "0.049", "-s", "1", "-x", "100", "-w", "410"]
 			, stdin=PIPE, stdout=PIPE, stderr=PIPE)
