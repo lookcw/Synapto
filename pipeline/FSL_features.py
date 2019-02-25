ad991e03f680031c5e2d54e330fb74dc5819dd16
lookcw <clook@terpmail.umd.edu>
2019-02-25 12:54:26 -0500

    changing parameters on FSL_features

--- pipeline/FSL_features.py
+++ pipeline/FSL_features.py
@@ -4,16 +4,16 @@ from subprocess import Popen, PIPE, call
 import numpy as np
 
 
-def extractFSLFeatures(time_series_electrode):
+def extractFSLFeatures(time_series_electrode,l=1, m=10, p=0.049, s=1, x=100, w=410):
     if "TERM_PROGRAM" in os.environ:
-        p = Popen(["./FSL_mac", "-l", "1", "-m", "10", "-p", "0.049", "-s",
-                   "1", "-x", "100", "-w", "410"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        p = Popen(["./FSL_mac", "-l", str(l), "-m", str(m), "-p", str(p), "-s",
+                   str(s), "-x", str(x), "-w", str(w)], stdin=PIPE, stdout=PIPE, stderr=PIPE)
         inMat = "\n".join([' '.join(x) for x in time_series_electrode])
         output, err = p.communicate(input=inMat)
         mat = [s.strip().split(' ') for s in output.strip().split('\n')]
     else:
-        p = Popen(["./FSL_linux", "-l", "1", "-m", "10", "-p", "0.049", "-s",
-                   "1", "-x", "100", "-w", "410"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        p = Popen(["./FSL_linux", "-l", str(l), "-m", str(m), "-p", str(p), "-s",
+                   str(s), "-x", str(x), "-w", str(w)], stdin=PIPE, stdout=PIPE, stderr=PIPE)
         inMat = "\n".join([','.join(x) for x in time_series_electrode])
         output, err = p.communicate(input=inMat)
         mat = [s.strip().split(' ') for s in output.strip().split('\n')]
