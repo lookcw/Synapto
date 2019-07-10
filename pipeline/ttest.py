53b32a181c9e3e3b7b02f952af407a369eb6ff51
Megha Guggari <mguggari@terpmail.umd.edu>
2019-07-10 07:43:42 -0700

    ttest file

--- pipeline/ttest.py
+++ pipeline/ttest.py
@@ -4,17 +4,29 @@ import os
 import scipy
 from scipy import stats
 
-data_path = 'SampleDataTesting'
-filename = 'Averages.csv'
-time_series = np.array(list(csv.reader(open(os.path.join(data_path, filename)))))
+data_path = 'FeatureSets'
+filename = 'DomFreq_Brazil1instances_1_epochs16000_timepoints.csv'
+time_series_brazil = np.array(list(csv.reader(open(os.path.join(data_path, filename)))))
+filename = 'DomFreq_AR1instances_1_epochs16000_timepoints.csv'
+time_series_ar = np.array(list(csv.reader(open(os.path.join(data_path, filename)))))
 
-y1 = scipy.stats.norm.pdf(time_series)
-print y1
 
-for col in range(time_series.shape[1]):
-	a = time_series[0][col]
-	b = time_series[1][col]
+num_cols = time_series_brazil.shape[1]
 
+for col in range(2, num_cols-1):
+	hc_brazil = time_series_brazil[1:13,col].astype(float)
+	hc_ar = time_series_ar[1:13,col].astype(float)
+	
+	ad_brazil = time_series_brazil[13:,col].astype(float)
+	ad_ar = time_series_ar[13:,col].astype(float)
+
+	twosample_results_hc = scipy.stats.ttest_ind(hc_brazil, hc_ar)
+	twosample_results_ad = scipy.stats.ttest_ind(ad_brazil, ad_ar)
+	
+	print twosample_results_hc
+	print twosample_results_ad
+
+	
 
 
 	
