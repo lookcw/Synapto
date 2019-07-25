164035bcf306ee347b6b9bf282408ecaafba08ad
Anoop Patel <Anoop@Anoops-MBP.home>
2019-07-24 22:02:10 -0400

    updating pipeline.py

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -18,16 +18,10 @@ from ASD_features import extractASDFeatures
 from createFeatureSet import createFeatureSet
 from createMatrixFeatureSet import createMatrixFeatureSet
 import pearson_features
-<<<<<<< HEAD
-import granger_features 
-import domFreq_features 
-import raw_features
-=======
 import granger_features
 import domFreq_features
 import domFreqVar_features
 # import feature_steepness
->>>>>>> 30f3d624f94c1d5bbd4e13d43192fb2e8ffb3ca1
 import FSL_features
 import pac_features
 from compute_score import compute_group_score
@@ -40,11 +34,7 @@ from feature_ranking import get_feature_importance
 from identifier import paramToFilename, recurrParamToFilename
 from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier, GradientBoostingClassifier
 from group import file_2_recurr_X
-<<<<<<< HEAD
-from SVD import svd
-=======
 from shuffle_data import shuffle_data
->>>>>>> 30f3d624f94c1d5bbd4e13d43192fb2e8ffb3ca1
 #from nn_Recurr import nn_Recurr
 
 
@@ -61,47 +51,6 @@ startAtFS = False
 FS = True  # feature selection
 RECURR = False
 
-<<<<<<< HEAD
-for i in range(1,len(sys.argv),2):
-	if str(sys.argv[i]) == "-h":
-		helpString = ('Run pipeline starting from beginning:\nInput arguments:\n-d: data type (choices: Brazil, Greece)' +
-		'\n-f: feature name (choices: ASD, Wavelet, FSL)\n-i: instances per patient (ex: 1)\n-t: number of time points per instance (ex: 60)' +
-		'\n-nfs: no feature selection\n-recurr: use LSTM' +
-		'\n\nRun Pipeline With Existing Feature Set:\nInput arguments:\n-fs: feature set (.../PathToFeatureSetFile)')
-		print(helpString)
-		sys.exit()
-	
-	elif str(sys.argv[i]) == "-d":
-		data_type = sys.argv[i+1]
-		if data_type != 'Brazil' and data_type != 'Greece' and data_type != 'newBrazil' and data_type != 'Neuronetrix':
-			print("Invalid type of data. Choose from list in help documentation")
-			sys.exit()
-	elif str(sys.argv[i]) == "-e":
-		epochs_per_instance = int(sys.argv[i+1])
-
-	elif str(sys.argv[i]) == "-f":
-		featureName = sys.argv[i+1]
-
-	elif str(sys.argv[i]) == "-i":
-		num_instances = int(sys.argv[i+1])
-		if num_instances == 0:
-			print("Did not input instances per patient argument (-p)")
-			sys.exit()
-
-	elif str(sys.argv[i]) == "-t":
-		num_timePoints = int(sys.argv[i+1])
-	elif str(sys.argv[i]) == "-nfs":
-		FS = False
-	elif str(sys.argv[i]) == "-recurr":
-		RECURR = True
-	elif str(sys.argv[i]) == "-fs":
-		features_path = sys.argv[i+1]
-		filename = features_path.split('/')[-1]
-		startAtFS = True
-	else:
-		print("Wrong format. Remember header must precede argument provided.\nUse -h for help.")
-		sys.exit()
-=======
 for i in range(1, len(sys.argv), 2):
     if str(sys.argv[i]) == "-h":
         helpString = ('Run pipeline starting from beginning:\nInput arguments:\n-d: data type (choices: Brazil, Greece)' +
@@ -162,117 +111,11 @@ for i in range(1, len(sys.argv), 2):
         print(sys.argv[i])
         print("Wrong format. Remember header must precede argument provided.\nUse -h for help.")
         sys.exit()
->>>>>>> 30f3d624f94c1d5bbd4e13d43192fb2e8ffb3ca1
 
 num_epochs = num_instances * epochs_per_instance
 
 # If starting at the beginning - at feature set creation
 if not startAtFS:
-<<<<<<< HEAD
-	if data_type == '':
-		print("Did not input data type. Choose from list in help documentation")
-		sys.exit()
-	if data_type != 'Brazil' and data_type != 'Greece' and data_type != 'newBrazil' and data_type != 'Neuronetrix':
-		print("Invalid type of data. Choose from list in help documentation")
-		sys.exit()
-	if not RECURR:
-		if featureName == '':
-			print("Did not input feature name argument (-f)")
-			#sys.exit()
-	if num_epochs == 0:
-		print("Did not input instances per patient argument (-i)")
-		#sys.exit()
-	if num_timePoints == 0:
-		print("Did not input time points argument (-t)\nUse -h for help.")
-		sys.exit()
-	if featureName == 'Raw':
-		extractFeatureFunc = functools.partial(createMatrixFeatureSet, raw_features, featureName)
-	if not RECURR:
-		if featureName == 'ASD':
-			extractFeatureFunc = extractASDFeatures
-		# elif featureName == 'Wavelet':
-			# extractFeatureFunc = extractWaveletFeatures
-		elif featureName == 'FSL':
-			extractFeatureFunc = functools.partial(createMatrixFeatureSet, FSL_features, featureName)
-		elif featureName == 'Pearson':
-			extractFeatureFunc = functools.partial(createMatrixFeatureSet, pearson_features, featureName)
-		elif featureName == 'Granger':
-			extractFeatureFunc = functools.partial(createMatrixFeatureSet, granger_features, featureName)
-		elif featureName == 'DomFreq':
-			extractFeatureFunc = functools.partial(createMatrixFeatureSet, domFreq_features, featureName)
-			# extractFeatureFunc = extractDomFreqFeatures
-		else:
-			print("Invalid feature name. Choose from list in help documentation")
-			sys.exit()
-
-	#feature extraction
-	print("Creating Feature Set...")
-
-	if (RECURR):
-		features_path = sys.path[0] + '/FeatureSets/' + recurrParamToFilename(featureName, data_type, num_instances ,num_timePoints, epochs_per_instance)
-	else:
-		#unique identifier for different input parameters
-		features_path = sys.path[0] + '/FeatureSets/'+  paramToFilename(featureName, data_type, num_instances ,num_timePoints, epochs_per_instance)
-		
-	#define features and reduced_features paths
-	
-	reduced_features_path = sys.path[0] + '/ReducedFeatureSets/'+ paramToFilename(featureName, data_type, num_instances ,num_timePoints, epochs_per_instance)
-	
-	#create feature set if does not exist in Feature Sets folder
-	if not os.path.exists(features_path):
-		print("feature file dne, making it now")
-		#3rd parameter is extractFeature function of choice
-		if (data_type == 'Brazil'):
-			data_folder_path1 = 'BrazilRawData/HCF50'
-			data_folder_path2 = 'BrazilRawData/ADF50'
-			num_electrodes = 21
-
-		if (data_type == 'Greece'):
-			data_folder_path1 = '.../PathToGreeceHC_DataFolder'
-			data_folder_path2 = '.../PathToGreeceMCI_DataFolder'
-			num_electrodes = 8
-
-		if (data_type == 'newBrazil'):
-			data_folder_path1 = 'BrazilRawData/HCF50_new'
-			data_folder_path2 = 'BrazilRawData/ADF50_new'
-			num_electrodes = 21
-
-		if (data_type == 'Greece'):
-			data_folder_path1 = '.../PathToGreeceHC_DataFolder'
-			data_folder_path2 = '.../PathToGreeceMCI_DataFolder'
-			num_electrodes = 8
-
-		if (data_type == 'Neuronetrix'):
-			data_folder_path1 = '/Users/Anoop/Documents/Synapto/pipeline/Neuronetrix/AD'
-			data_folder_path2 = '/Users/Anoop/Documents/Synapto/pipeline/Neuronetrix/HC'
-			num_electrodes = 21
-			
-		if (featureName == 'FSL' or featureName == 'Pearson' or featureName == 'Granger' or featureName == 'DomFreq' or featureName == 'TsFresh'):
-			extractFeatureFunc(num_instances ,num_timePoints, epochs_per_instance, data_folder_path1, data_folder_path2, features_path, RECURR)
-			# extractFeatureFunc(num_epochs, num_timePoints, data_folder_path1, data_folder_path2, data_type, RECURR)
-		elif (RECURR):
-			extractFeatureFunc(num_instances ,num_timePoints, epochs_per_instance, data_folder_path1, data_folder_path2, features_path, RECURR)
-			#createFeatureSet(num_epochs, num_timePoints, '', '', num_electrodes, 
-			#	data_folder_path1, data_folder_path2, data_type, features_path, RECURR)
-		else:
-			createFeatureSet(num_epochs, num_timePoints, featureName, extractFeatureFunc, num_electrodes, 
-				data_folder_path1, data_folder_path2, data_type, features_path, RECURR)
-
-	else:
-		print("Feature set already exists: " + features_path)
-
-	if (data_type == 'Brazil'):
-		num_electrodes = 21
-	if (data_type == 'Greece'):
-		num_electrodes = 8
-	
-	print(features_path)
-	data = pd.read_csv(features_path, header = 'infer', delimiter=',')
-else: #starting pipeline with feature selection
-	data = pd.read_csv(features_path, header = 'infer')
-
-#shuffle rows of dataframe
-=======
     if data_type == '' and not data_folder_path1 and not data_folder_path2:
         print("Did not input data type. Choose from list in help documentation")
         sys.exit()
@@ -404,7 +247,6 @@ else:  # starting pipeline with feature selection
     data = pd.read_csv(features_path, header='infer')
 
 # shuffle rows of dataframe
->>>>>>> 30f3d624f94c1d5bbd4e13d43192fb2e8ffb3ca1
 data = shuffle(data)
 data.sample(frac=1).reset_index(drop=True)
 # obtain Y using last column
@@ -460,29 +302,14 @@ o_filename = 'output_pipeline.csv'
 # Megha's svm
 #svm_func(X_reduced,y,num_seeds, num_folds, 'output_pipeline.csv')
 
-<<<<<<< HEAD
-#nn_keras
-nn = nn_keras(X, y, n_hlayers = 3, neurons = [100,100,100],learning_rate = 0.1,n_folds =180,n_classes = 2, seed = 5, grps = groups)
-=======
 # nn_keras
 # nn = nn_keras(X, y, n_hlayers = 3, neurons = [100,100,100],learning_rate = 0.1,n_folds =3,n_classes = 2, seed = 5, grps = groups)
->>>>>>> 30f3d624f94c1d5bbd4e13d43192fb2e8ffb3ca1
 
 # nn_Recurr
 if (RECURR):
-<<<<<<< HEAD
-	print(features_path)
-	svd(features_path, 2)
-	SVD_features_path = features_path.split('.')[0] + '_SVD.' + features_path.split('.')[1]
-	patient_num, X_3D, y_ = file_2_recurr_X(SVD_features_path)
-	#print("SHAPE")
-	#print(X_3D.shape)
-	nn_recurr = nn_Recurr(X_3D, y, n_hlayers = 3, neurons = [100,50,20],learning_rate = 0.1,n_folds =2,n_classes = 2, seed = 5)
-=======
     X_3D, y_ = file_2_recurr_X(features_path)
     nn_recurr = nn_Recurr(X_3D, y, n_hlayers=3, neurons=[
                           100, 100, 100], learning_rate=0.1, n_folds=2, n_classes=2, seed=5)
->>>>>>> 30f3d624f94c1d5bbd4e13d43192fb2e8ffb3ca1
 
 # various sklearn models
 logreg = LogisticRegression()
