ed5fb775ad2ea90ef6b27269a16390cc10a8e588
Anoop Patel <Anoop@wireless-10-105-23-93.umd.edu>
2019-09-30 20:16:41 -0400

    updating gridsearch

--- pipeline/record_results.py
+++ pipeline/record_results.py
@@ -146,7 +146,7 @@ def get_results(clf, df, config, config_features):
 
 # define function to compute cross validation score
 def _compute_group_score(clf, df, num_folds, scoring='accuracy', nn_model=[]):
-    print(df['e1_e2'])
+    #print(df['e1_e2'])
     (X, y, groups, instance_num) = _split_dataframe(df)
     print(X.shape)
     if "keras" in str(clf):
