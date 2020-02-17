59b126ca1aab0ff8e758b9e81888c640f9e10081
Megha Guggari <mguggari@terpmail.umd.edu>
2020-02-17 17:28:38 -0500

    working on FSL features average

---
+++ pipeline/FSL_features_avg.py
@@ -0,0 +1,30 @@
+import os
+import sys
+from subprocess import Popen, PIPE, call
+import numpy as np
+from headers import compareHeader
+
+def getHeader(time_series_electrode):
+    return compareHeader(time_series_electrode)
+
+def extractFeatures(time_series_electrode, config_feature):
+    time_series_electrode = time_series_electrode.astype(str)
+    if "TERM_PROGRAM" in os.environ:
+        p = Popen(["./FSL_mac", "-l", "1", "-m", "10", "-p", "0.049", "-s",
+                   "1", "-x", "200", "-w", "2000"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        inMat = "\n".join([','.join(x) for x in time_series_electrode]).encode('utf-8')
+        output, err = p.communicate(input=inMat)
+        mat = [s.strip().split(' ') for s in output.decode().strip().split('\n')]
+    else:
+        p = Popen(["./FSL_linux", "-l", "1", "-m", "10", "-p", "0.049", "-s",
+                   "1", "-x", "200", "-w", "2000"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        inMat = "\n".join([','.join(x) for x in time_series_electrode]).encode('utf-8')
+        output, err = p.communicate(input=inMat)
+        mat = [s.strip().split(' ') for s in output.decode().strip().split('\n')]
+    
+    return list(map(float, vec))
+
+def config_to_filename(config_feature):
+    return str(config_feature['l']) + "_l_" + str(config_feature['m']) + "_m_" + str(config_feature['p']) + '_p_' + str(config_feature['s']) + '_s_' + str(config_feature['x']) + '_x_' + str(config_feature['w']) + '_w_.csv'
+    
+    
--- pipeline/average_features.py
+++ pipeline/average_features.py
@@ -1,8 +1,11 @@
+import os
 import pandas as pd
 import argparse
 import seaborn as sns
 import matplotlib.pyplot as plt 
 from identifier import filenameToParam
+
+# AVERAGE_FEATURE_SET_FOLDER = '/Users/megha/Synapto/pipeline/AverageFeatures/'
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", help="input feature file to save model for")
@@ -14,7 +17,11 @@ print(feature_name)
 dataset = pd.read_csv(in_filename).drop(columns=['instance code','patient num','instance num'])
 meaned = dataset.groupby(['class']).mean()
 data = meaned
-data.insert(1, "name_of_feature", feature_name)
+data.insert(0, "name_of_feature", feature_name)
 print(data)
-data.to_csv('averages.csv')
+name_csv = feature_name + "_averages.csv"
 
+# name_csv = AVERAGE_FEATURE_SET_FOLDER + feature_name + "_averages.csv"
+data.to_csv(name_csv)
+
+# os.path.join(AVERAGE_FEATURE_SET_FOLDER, name_csv)
